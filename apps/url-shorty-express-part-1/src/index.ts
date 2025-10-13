a44f0be1d127f81a6e05ff38e872cbaf408578ce
polubis <polubis@wp.pl>
2025-10-13 11:35:53 +0200

    fix(app): error with error-async-middleware

--- apps/url-shorty-express-part-1/src/index.ts
+++ apps/url-shorty-express-part-1/src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import "express-async-errors";
 import express from "express";
 import { shortUrlSchema } from "./ipc/payloads";
 import { validate } from "./cross-shell/middleware/validate";
